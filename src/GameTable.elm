2aeaa15e10b2c1e241fa253a16a5958a8c0bed28
Gustavo Martins <martins.yo@gmail.com>
2020-06-15 18:18:46 +0200

    Uses elm-ui for html, added dummy player statuses

--- src/GameTable.elm
+++ src/GameTable.elm
@@ -1,6 +1,10 @@
 module GameTable exposing (Model, Msg, initState, update, view)
 
-import Html exposing (..)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Input as Input
+import Html exposing (Html)
 import Html.Events exposing (..)
 
 
@@ -86,6 +90,15 @@ update model msg =
 
 view : Model -> Html Msg
 view model =
+    layout [] <|
+        column []
+            [ viewGameTable model.state
+            , showStatus
+            ]
+
+
+
+{--
     div []
         [ viewGameTable model.state
         , hr [] []
@@ -93,9 +106,24 @@ view model =
         , button [ onClick GoPassive ] [ text "Go passive" ]
         , button [ onClick GoActive ] [ text "Go active" ]
         ]
+        --}
 
 
-viewGameTable : State -> Html Msg
+showStatus : Element Msg
+showStatus =
+    column []
+        [ paragraph [] [ text "Marina: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Paulo: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Sarra: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Gustavo: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Marua: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Diego: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Talita: 5 money, 5 on gren, 3 on blue" ]
+        , paragraph [] [ text "Thiago: 5 money, 5 on gren, 3 on blue" ]
+        ]
+
+
+viewGameTable : State -> Element Msg
 viewGameTable state =
     case state of
         Q0 ->
@@ -132,96 +160,172 @@ viewGameTable state =
 --}
 
 
-viewStateQ0 : Html Msg
+viewStateQ0 : Element Msg
 viewStateQ0 =
-    div []
-        [ text "All five camels are sleeping and you have to wake them up. Roll the dices until they arent asleep anymore. Please, hurry up! Other players are waiting for you :)"
-        , hr [] []
-        , p [] [ text "Simulate user command" ]
-        , button [ onClick Warmup ] [ text "Warm a camel up" ]
-        , hr [] []
-        , p [] [ text "Simulate a server command" ]
-        , button [ onClick Start ] [ text "Start" ]
+    Element.column [ width fill ]
+        [ Element.paragraph []
+            [ Element.text "Q0: All five camels are sleeping and you have to wake them up. Roll the dices until they arent asleep anymore. Please, hurry up! Other players are waiting for you :)"
+            ]
+        , Element.paragraph [] [ text "Simulate user command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just Warmup
+            , label = el [] <| text "Warm camel up"
+            }
+        , paragraph [] [ text "Simulate a server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just Start
+            , label = el [] <| text "Start"
+            }
         ]
 
 
-viewStateQ0_passive : Html msg
+viewStateQ0_passive : Element msg
 viewStateQ0_passive =
-    div []
+    text "Workflow view Q0 passive"
+
+
+
+{--div []
         [ text "Other player is waking up the camels. Please, have a little patience"
         ]
+        --}
 
 
-viewStateQ1 : Html Msg
+viewStateQ1 : Element Msg
 viewStateQ1 =
-    div []
-        [ text "You can now get a point by shaking the dice, or get no points now and try to bet in a camel for the current leg, or put a mirage tile to annoy other camels and get some money if they fall there. You can also put a oasis tile to help some camel (you also get a point if he lands there), or bet on the final winner or looser"
-        , hr [] []
-        , p [] [ text "Simulate an user command" ]
-        , button [] [ text "Shake a dice" ]
-        , button [] [ text "Put a desert tile" ]
-        , button [] [ text "Bet on a camel for this leg" ]
-        , button [] [ text "Bet who will win final round" ]
-        , button [] [ text "Bet on last camel on final round" ]
-        , hr [] []
-        , p [] [ text "Simulate a server command" ]
-        , button [ onClick LastDiceThrown ] [ text "Leg finished" ]
-        , button [ onClick FinishedRace ] [ text "Finish race" ]
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q1: You can now get a point by shaking the dice, or get no points now and try to bet in a camel for the current leg, or put a mirage tile to annoy other camels and get some money if they fall there. You can also put a oasis tile to help some camel (you also get a point if he lands there), or bet on the final winner or looser"
+            ]
+        , paragraph [] [ text "Simulate an user command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Nothing
+            , label = el [] <| text "Shake a dice"
+            }
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Nothing
+            , label = el [] <| text "Put a desert tile"
+            }
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Nothing
+            , label = el [] <| text "Bet on legs winner"
+            }
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Nothing
+            , label = el [] <| text "Bet on final winner"
+            }
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Nothing
+            , label = el [] <| text "Bet on final looser"
+            }
+        , paragraph [] [ text "Simulate a server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just LastDiceThrown
+            , label = el [] <| text "Leg finished"
+            }
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just FinishedRace
+            , label = el [] <| text "Finish race"
+            }
         ]
 
 
-viewStateQ2 : Html Msg
+viewStateQ2 : Element Msg
 viewStateQ2 =
-    div []
-        [ text "That was an amazing race and you can now get money for your bets on the previous leg!"
-        , hr [] []
-        , p [] [ text "Simulate server command:" ]
-        , button [ onClick GotLegMoney ] [ text "Got leg money" ]
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q2: That was an amazing race and you can now get money for your bets on the previous leg!"
+            ]
+        , paragraph [] [ text "Simulate an server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just GotLegMoney
+            , label = el [] <| text "Got leg money"
+            }
         ]
 
 
-viewStateQ3 : Html Msg
+viewStateQ3 : Element Msg
 viewStateQ3 =
-    div []
-        [ text "You got some money, but now as I said its time go get much more!! Who bet corretly on the first winner? Better yet, who was the FIRST to bet correctly? You can get up to 8 points if you were the earliest lucky one!"
-        , hr [] []
-        , p [] [ text "Simulate server command:" ]
-        , button [ onClick GotFinalWinnerMoney ] [ text "Got final winner money" ]
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q3: You got some money, but now as I said its time go get much more!! Who bet corretly on the first winner? Better yet, who was the FIRST to bet correctly? You can get up to 8 points if you were the earliest lucky one!"
+            ]
+        , paragraph [] [ text "Simulate an server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just GotFinalWinnerMoney
+            , label = el [] <| text "Got final winner money"
+            }
         ]
 
 
-viewStateQ4 : Html Msg
+viewStateQ4 : Element Msg
 viewStateQ4 =
-    div []
-        [ text "Maybe you got money,maybe you didnt. But as far as good news go, you have yet one chance more! Because betting on the final looser can also give you great mone!"
-        , hr [] []
-        , p [] [ text "Simulate server command:" ]
-        , button [ onClick GotFinalLooserMoney ] [ text "Got final looser money" ]
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q4: Maybe you got money,maybe you didnt. But as far as good news go, you have yet one chance more! Because betting on the final looser can also give you great mone!"
+            ]
+        , paragraph [] [ text "Simulate an server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just GotFinalLooserMoney
+            , label = el [] <| text "Got final looser money"
+            }
         ]
 
 
-viewStateQ5 : Html Msg
+viewStateQ5 : Element Msg
 viewStateQ5 =
-    div []
-        [ text "Ha!! Great game, isnt it? I hope you enjoyed! Tell to your friends about it and see you next time!"
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q5: Ha!! Great game, isnt it? I hope you enjoyed! Tell to your friends about it and see you next time!"
+            ]
         ]
 
 
-viewStateQ6 : Html Msg
+viewStateQ6 : Element Msg
 viewStateQ6 =
-    div []
-        [ text "After many ups and downs, the race is finally finished! After collecing this legs money, bigger prizes are to come! If you got the final winner or final looser right, your pocket will leave you feeling you carry a stone of gold :)"
-        , hr [] []
-        , p [] [ text "Simulate server command:" ]
-        , button [ onClick GotLegMoney ] [ text "Got leg money" ]
+    Element.column [ spacingXY 0 10 ]
+        [ Element.paragraph []
+            [ Element.text "Q6: After many ups and downs, the race is finally finished! After collecing this legs money, bigger prizes are to come! If you got the final winner or final looser right, your pocket will leave you feeling you carry a stone of gold :)"
+            ]
+        , paragraph [] [ text "Simulate an server command" ]
+        , Input.button
+            [ Background.color (rgb255 0 255 0)
+            ]
+            { onPress = Just GotLegMoney
+            , label = el [] <| text "Got leg money"
+            }
         ]
 
 
-viewStateQError : Html msg
+viewStateQError : Element msg
 viewStateQError =
-    div [] [ text "Error state reached inside GameTable" ]
+    text "Error state reached inside GameTable"
 
 
-viewNotImplemented : Html msg
+viewNotImplemented : Element msg
 viewNotImplemented =
-    div [] [ text "View not implemented" ]
+    text "View not implemented on workflow"
