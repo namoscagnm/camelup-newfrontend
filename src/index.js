f42f8ca0587cbae51d435f4146bbb062c7e17611
Gustavo Martins <martins.yo@gmail.com>
2020-07-11 21:06:20 +0200

    autoformat on index.js

--- src/index.js
+++ src/index.js
@@ -1,38 +1,55 @@
-
-import './main.css';
-import { Elm } from './Main.elm';
-import * as serviceWorker from './serviceWorker';
-
-
+import "./main.css";
+import { Elm } from "./Main.elm";
+import * as serviceWorker from "./serviceWorker";
 
 const currentLocation = window.location;
-const backend_host_dev = "localhost:4000"
-const backend_host_prod = "camelup.gigalixirapp.com"
+const backend_host_dev = "localhost:4000";
+const backend_host_prod = "camelup.gigalixirapp.com";
 
-const backend_host_ws = (currentLocation.hostname === "localhost") ? ("ws://" + backend_host_dev) : ("wss://" + backend_host_prod)
-const backend_host_http = (currentLocation.hostname === "localhost") ? ("http://" + backend_host_dev) : ("https://" + backend_host_prod)
+const backend_host_ws =
+  currentLocation.hostname === "localhost"
+    ? "ws://" + backend_host_dev
+    : "wss://" + backend_host_prod;
+const backend_host_http =
+  currentLocation.hostname === "localhost"
+    ? "http://" + backend_host_dev
+    : "https://" + backend_host_prod;
 
 console.log(currentLocation);
 console.log(backend_host_ws);
 console.log(backend_host_http);
 
-
-const { Socket } = require('phoenix-channels')
-
+const { Socket } = require("phoenix-channels");
 
 const socket = new Socket(backend_host_ws + "/socket", {
   params: { token: window.userToken },
-  logger: (kind, msg, data) => { console.log('${kind}: ${msg}', data) }
-})
+  logger: (kind, msg, data) => {
+    console.log("${kind}: ${msg}", data);
+  },
+});
 
-const channel = socket.channel("games:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const channel = socket.channel("games:1", {});
+channel
+  .join()
+  .receive("ok", (resp) => {
+    console.log("Joined successfully", resp);
+  })
+  .receive("error", (resp) => {
+    console.log("Unable to join", resp);
+  });
 
 socket.connect();
 
 const app = Elm.Main.init({
-  node: document.getElementById('root'),
-  flags: backend_host_http
+  node: document.getElementById("root"),
+  flags: backend_host_http,
+});
+
+channel.on("broadcast_game_table", (payload) => {
+  console.log(
+    `JS: Receiving ${payload} game table data from Phoenix`,
+    payload.gameTable
+  );
+  app.ports.receiveGameTable.send({ gameTable: payload.gameTable });
+  console.log("JS finished sending game table to Elm");
 });
