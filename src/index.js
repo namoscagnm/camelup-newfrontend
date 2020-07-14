5028547ce0c684eb09ddd47d1809825ae6b9679a
Gustavo Martins <martins.yo@gmail.com>
2020-07-14 19:10:06 +0200

    Joins rooms only when asked

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import "./main.css";
 import { Elm } from "./Main.elm";
 import * as serviceWorker from "./serviceWorker";
 
-const currentLocation = window.location;
+const currentLocation_hostname = window.location.hostname;
 const backend_host_dev = "localhost:4000";
 const backend_host_prod = "camelup.gigalixirapp.com";
 
 const backend_host_ws =
-  currentLocation.hostname === "localhost"
-    ? "ws://" + backend_host_dev
+  currentLocation_hostname !== "camelup.zeit.sh"
+    ? "ws://" + currentLocation_hostname + ":4000"
     : "wss://" + backend_host_prod;
 const backend_host_http =
-  currentLocation.hostname === "localhost"
-    ? "http://" + backend_host_dev
+  currentLocation_hostname !== "camelup.zeit.sh"
+    ? "http://" + backend_host_dev + ":4000"
     : "https://" + backend_host_prod;
 
-console.log(currentLocation);
+console.log(currentLocation_hostname);
 console.log(backend_host_ws);
 console.log(backend_host_http);
 
@@ -32,29 +32,29 @@ const app = Elm.Main.init({
   node: document.getElementById("root"),
   flags: backend_host_http,
 });
-//var channel = socket.channel("games:lobby", {});
-
-//app.ports.joinRoom.subscribe(function (room) {
 
 let room = "1";
 let channel = socket.channel("games:" + room);
-console.log(`JS will ask Phoenix to join room: `, room);
-channel
-  .join()
-  .receive("ok", (resp) => {
-    console.log("Joined successfully room " + room, resp);
-  })
-  .receive("error", (resp) => {
-    console.log("Unable to join", resp);
-  })
-  .receive("timeout", () =>
-    console.log(
-      "Networking issue trying to connect to room " +
-        room +
-        ". Still waiting..."
-    )
-  );
-//});
+
+app.ports.joinRoom.subscribe(function (room) {
+  console.log(`JS will ask Phoenix to join room: `, room);
+  channel
+    .join()
+    .receive("ok", (resp) => {
+      console.log("Joined successfully room " + room, resp);
+    })
+    .receive("error", (resp) => {
+      console.log("Unable to join", resp);
+    })
+    .receive("timeout", () =>
+      console.log(
+        "Networking issue trying to connect to room " +
+          room +
+          ". Still waiting..."
+      )
+    );
+});
+
 channel.on("broadcast_game_table", (payload) => {
   console.log(`JS: Receiving ${payload} game table data from Phoenix`, payload);
   app.ports.receiveGameTableFromServer.send(payload);
